c0129c9c17e4337e99c8ddddf8b29df7c748af96
MDurbin <matthew.durbin@gmail.com>
2017-05-12 12:26:27 -0600

    A Java program to test the strength of a password
    Building the backbone of the program.

--- PasswordStrTester.java
+++ PasswordStrTester.java
@@ -1,17 +1,46 @@
 package PasswordStrengthTester;
 
+import javax.swing.JOptionPane;
+
 /**
  * @author Matthew Durbin
  *
  */
 public class PasswordStrTester {
+	
+	int[] charValue;
 
+	public void setValue(char val) {
+		
+		for(int i = 0; i > charValue.length; i++) {
+			if(Character.isLetter(val) && Character.isLowerCase(val)) {
+				charValue[i] = 1;
+			}else if(Character.isLetter(val) && Character.isUpperCase(val)) {
+				charValue[i] = 2;
+			}else if(Character.isDigit(val)) {
+				charValue[i] = 1;
+			}else if(Character.isSpaceChar(val)) {
+				charValue[i] = 2;
+			}else{
+				charValue[i] = 2;
+			}
+		}
+		
+	}
+	
 	/**
-	 * @param args
+	 * 
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int stringLen = 0;
+		
+		String password = JOptionPane.showInputDialog(null, "Enter the password you would like to test:", JOptionPane.QUESTION_MESSAGE);
+		
+		stringLen = password.length();
+		
+		
+		
 	}
 
 }
